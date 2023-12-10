6f977b05ab5b20ed48953fe3841501a8f4238732
Martin Rehberger <dev@kero.codes>
2023-12-10 21:27:22 +0700

    Add skeleton day04

--- 2023/go/main.go
+++ 2023/go/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/kingkero/adventofcode/2023/go/day01"
 	"github.com/kingkero/adventofcode/2023/go/day02"
 	"github.com/kingkero/adventofcode/2023/go/day03"
+	"github.com/kingkero/adventofcode/2023/go/day04"
 )
 
 func main() {
@@ -21,4 +22,8 @@ func main() {
 	start = time.Now()
 	p01, p02 = day03.Solve("./day03/input.txt")
 	fmt.Printf("Day 3:\t%v\t/\t%v\ttook %v\n", p01, p02, time.Since(start))
+
+	start = time.Now()
+	p01, p02 = day04.Solve("./day04/input.txt")
+	fmt.Printf("Day 4:\t%v\t/\t%v\ttook %v\n", p01, p02, time.Since(start))
 }
