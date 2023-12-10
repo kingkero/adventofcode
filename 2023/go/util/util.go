14312a4275362035f897752b39d648f09f9a3db9
Martin Rehberger <dev@kero.codes>
2023-12-10 21:30:21 +0700

    Move ReadFile and SumInts to util package

---
+++ 2023/go/util/util.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+// Read a complete file line by line into memory.
+func ReadFile(input string) ([]string, error) {
+	file, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
+}
+
+// Get the sum of all ints in an array.
+func SumInts(values []int) int {
+	result := 0
+	for _, val := range values {
+		result += val
+	}
+	return result
+}
