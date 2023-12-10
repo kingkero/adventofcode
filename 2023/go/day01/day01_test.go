e6476a2efb033a983fc5369327ae854a17bd4540
Martin Rehberger <dev@kero.codes>
2023-12-10 18:23:32 +0700

    Move test solutions to variables

--- 2023/go/day01/day01_test.go
+++ 2023/go/day01/day01_test.go
@@ -3,13 +3,14 @@ package day01
 import "testing"
 
 func TestSolutionDay01(t *testing.T) {
+	solution01, solution02 := 55208, 54578
 	part01, part02 := Solve("./input.txt")
 
-	if part01 != 55208 {
-		t.Fatalf("Part01 solution expected %d, was %d", 55208, part01)
+	if part01 != solution01 {
+		t.Fatalf("Part01 solution expected %d, was %d", solution01, part01)
 	}
 
-	if part02 != 54578 {
-		t.Fatalf("Part02 solution expected %d, was %d", 54578, part02)
+	if part02 != solution02 {
+		t.Fatalf("Part02 solution expected %d, was %d", solution02, part02)
 	}
 }
