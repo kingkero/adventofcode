d8ffcadc06f04753444e704b193ea8a2770dec6b
Martin Rehberger <dev@kero.codes>
2023-12-10 21:59:12 +0700

    Solve day 04 part 01

--- 2023/go/day04/day04.go
+++ 2023/go/day04/day04.go
@@ -1,7 +1,48 @@
 package day04
 
+import (
+	"log"
+	"math"
+	"regexp"
+	"strconv"
+
+	"github.com/kingkero/adventofcode/2023/go/util"
+)
+
 func part01(lines []string) int {
 	result := 0
+
+	whiteSpaceRegex := regexp.MustCompile("\\s+")
+
+	for _, line := range lines {
+		parts := whiteSpaceRegex.Split(line, -1)
+
+		// cardNumber := parts[1]
+		var winners []int
+		amountOfWinners := 0
+
+		isWinner := true
+		for i := 2; i < len(parts); i++ {
+			if parts[i] == "|" {
+				isWinner = false
+				continue
+			}
+
+			number, _ := strconv.ParseInt(parts[i], 10, 64)
+			if isWinner {
+				winners = append(winners, int(number))
+				continue
+			}
+
+			if util.IntsContains(winners, int(number)) {
+				amountOfWinners++
+			}
+		}
+
+		if amountOfWinners > 0 {
+			result += int(math.Pow(2.0, float64(amountOfWinners-1)))
+		}
+	}
 
 	return result
 }
@@ -13,6 +54,10 @@ func part02(lines []string) int {
 }
 
 func Solve(input string) (int, int) {
-	lines := make([]string, 0)
+	lines, err := util.ReadFile(input)
+	if err != nil {
+		log.Fatal("Could not open file "+input, err)
+	}
+
 	return part01(lines), part02(lines)
 }
--- 2023/go/util/util.go
+++ 2023/go/util/util.go
@@ -33,3 +33,12 @@ func SumInts(values []int) int {
 	}
 	return result
 }
+
+func IntsContains(haystack []int, needle int) bool {
+	for _, val := range haystack {
+		if val == needle {
+			return true
+		}
+	}
+	return false
+}
