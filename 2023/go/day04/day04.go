4acbfeb54451099fa1f48f15e7b6296ab70b15c8
Martin Rehberger <dev@kero.codes>
2023-12-10 21:27:22 +0700

    Add skeleton day04

---
+++ 2023/go/day04/day04.go
@@ -0,0 +1,18 @@
+package day04
+
+func part01(lines []string) int {
+	result := 0
+
+	return result
+}
+
+func part02(lines []string) int {
+	result := 0
+
+	return result
+}
+
+func Solve(input string) (int, int) {
+	lines := make([]string, 0)
+	return part01(lines), part02(lines)
+}
