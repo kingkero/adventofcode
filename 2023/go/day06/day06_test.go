60fa60130042825c82d6407cde2b55af84bece68
Martin Rehberger <dev@kero.codes>
2023-12-11 20:29:52 +0700

    Add correct value for testing day 06 part 01

--- 2023/go/day06/day06_test.go
+++ 2023/go/day06/day06_test.go
@@ -3,7 +3,7 @@ package day06
 import "testing"
 
 func TestSolutionDay06(t *testing.T) {
-	solution01, solution02 := -1, -1
+	solution01, solution02 := 140220, -1
 	part01, part02 := Solve("./input.txt")
 
 	if part01 != solution01 {
