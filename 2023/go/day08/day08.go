a241c5d833333dfc8e130271d4583ee508dc8e38
Martin Rehberger <dev@kero.codes>
2023-12-15 00:14:45 +0700

    Make part01 more dynamic

--- 2023/go/day08/day08.go
+++ 2023/go/day08/day08.go
@@ -14,24 +14,35 @@ type Node struct {
 	leftData  string
 	right     *Node
 	rightData string
+	isEndNote bool
+}
+
+func (node *Node) step(direction string) *Node {
+	if direction == "L" {
+		return node.left
+	}
+
+	return node.right
 }
 
 func getMinimumSteps(instructions []string, nodes map[string]*Node) int {
 	checkNode := nodes["AAA"]
 	steps := 0
 	for {
-		if instructions[steps%len(instructions)] == "L" {
-			checkNode = checkNode.left
-		} else {
-			checkNode = checkNode.right
-		}
+		checkNode = checkNode.step(instructions[steps%len(instructions)])
 
-		if checkNode.data == "ZZZ" {
+		if checkNode.isEndNote {
 			return steps + 1
 		}
 
 		steps++
 	}
+}
+
+func getRules()
+
+func getMinimumStepsParallel(instructions []string, nodes map[string]*Node, startNodes []*Node) int {
+	return 0
 }
 
 func part01(lines []string) int {
@@ -48,7 +59,7 @@ func part01(lines []string) int {
 		left := matches[0][2]
 		right := matches[0][3]
 
-		node := &Node{value, nil, left, nil, right}
+		node := &Node{value, nil, left, nil, right, value == "ZZZ"}
 		nodes[value] = node
 	}
 
@@ -61,7 +72,34 @@ func part01(lines []string) int {
 }
 
 func part02(lines []string) int {
-	return 0
+	rules := lines[2:]
+
+	lineParser := regexp.MustCompile("([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)")
+
+	nodes := make(map[string]*Node)
+	var startNodes []*Node
+
+	for _, line := range rules {
+		matches := lineParser.FindAllStringSubmatch(line, -1)
+
+		value := matches[0][1]
+		left := matches[0][2]
+		right := matches[0][3]
+
+		node := &Node{value, nil, left, nil, right, value[2:] == "Z"}
+		nodes[value] = node
+
+		if value[2:] == "A" {
+			startNodes = append(startNodes, node)
+		}
+	}
+
+	for _, node := range nodes {
+		node.left = nodes[node.leftData]
+		node.right = nodes[node.rightData]
+	}
+
+	return getMinimumStepsParallel(strings.Split(lines[0], ""), nodes, startNodes)
 }
 
 func Solve(input string) (int, int) {
