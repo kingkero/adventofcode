5a087faf670ba3cf3892c670d7a444b59b1bf79f
Martin Rehberger <dev@kero.codes>
2023-12-20 18:36:24 +0700

    WIP: Day 11 part 01: get expanded field

--- 2023/go/day11/day11.go
+++ 2023/go/day11/day11.go
@@ -1,13 +1,88 @@
 package day11
 
 import (
+	"fmt"
 	"log"
+	"slices"
+	"strings"
 
 	"github.com/kingkero/adventofcode/2023/go/util"
 )
 
+func isRowEmpty(row []string) bool {
+	for col := 0; col < len(row); col++ {
+		if row[col] != "." {
+			return false
+		}
+	}
+	return true
+}
+
+func isColEmpty(fields [][]string, col int) bool {
+	for row := 0; row < len(fields); row++ {
+		if fields[row][col] != "." {
+			return false
+		}
+	}
+	return true
+}
+
+func getExpandedFields(lines []string) [][]string {
+	var expandRows, expandCols []int
+
+	fields := make([][]string, len(lines))
+	for row := 0; row < len(lines); row++ {
+		fields[row] = strings.Split(lines[row], "")
+
+		if isRowEmpty(fields[row]) {
+			expandRows = append(expandRows, row+len(expandRows))
+		}
+	}
+
+	for col := 0; col < len(fields[0]); col++ {
+		if isColEmpty(fields, col) {
+			expandCols = append(expandCols, col+len(expandCols))
+		}
+	}
+
+	newRows := len(lines) + len(expandRows)
+	newCols := len(lines[0]) + len(expandCols)
+
+	expandedFields := make([][]string, newRows)
+
+	referenceRow := 0
+	for row := 0; row < newRows; row++ {
+		newRow := make([]string, newCols)
+		referenceCol := 0
+		for col := 0; col < newCols; col++ {
+			newRow[col] = fields[referenceRow][referenceCol]
+			referenceCol++
+
+			if slices.Contains(expandCols, col) {
+				col++
+				newRow[col] = "."
+			}
+		}
+		expandedFields[row] = newRow
+		referenceRow++
+
+		if slices.Contains(expandRows, row) {
+			row++
+			expandedFields[row] = strings.Split(strings.Repeat(".", newCols), "")
+		}
+	}
+
+	return expandedFields
+}
+
 func part01(lines []string) int {
 	result := 0
+
+	field := getExpandedFields(lines)
+
+	for _, row := range field {
+		fmt.Println(strings.Join(row, ""))
+	}
 
 	return result
 }
