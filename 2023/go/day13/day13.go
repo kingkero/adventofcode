58a49b97135fbae72b93ab738045bd1b66dc4aa3
Martin Rehberger <dev@kero.codes>
2023-12-26 19:24:37 +0700

    Day 13: Add Matrix struct and allow to rotate

--- 2023/go/day13/day13.go
+++ 2023/go/day13/day13.go
@@ -2,11 +2,56 @@ package day13
 
 import (
 	"log"
+	"strings"
 
 	"github.com/kingkero/adventofcode/2023/go/util"
 )
 
-func part01(lines []string) int {
+func NewMatrix(lines []string) *Matrix {
+	height := len(lines)
+	width := len(lines[0])
+
+	data := make([][]string, height)
+	for i, line := range lines {
+		data[i] = strings.Split(line, "")
+	}
+
+	return &Matrix{data, width, height}
+}
+
+func ParseMatrixes(lines []string) []*Matrix {
+	var matrixes []*Matrix
+
+	startIndex := 0
+	for i, line := range lines {
+		if line == "" {
+			matrixes = append(matrixes, NewMatrix(lines[startIndex:(i-startIndex)]))
+			startIndex = i + 1
+		}
+	}
+	matrixes = append(matrixes, NewMatrix(lines[startIndex:]))
+	return matrixes
+}
+
+type Matrix struct {
+	data   [][]string
+	width  int
+	height int
+}
+
+func (original Matrix) rotate() *Matrix {
+	data := make([][]string, original.width)
+	for row := 0; row < original.width; row++ {
+		line := make([]string, original.height)
+		for col := 0; col < original.height; col++ {
+			line[col] = original.data[col][original.width-row-1]
+		}
+		data[row] = line
+	}
+	return &Matrix{data, original.height, original.width}
+}
+
+func part01(matrixes []*Matrix) int {
 	return 0
 }
 
@@ -20,5 +65,7 @@ func Solve(input string) (int, int) {
 		log.Fatal("Could not open file "+input, err)
 	}
 
-	return part01(lines), part02(lines)
+	matrixes := ParseMatrixes(lines)
+
+	return part01(matrixes), part02(lines)
 }
