25b98c20d776fa09c66ce9b0d3f30a640bb71fca
Martin Rehberger <dev@kero.codes>
2023-12-26 17:42:22 +0700

    Add day 23 skeleton

---
+++ 2023/go/day23/day23.go
@@ -0,0 +1,24 @@
+package day23
+
+import (
+	"log"
+
+	"github.com/kingkero/adventofcode/2023/go/util"
+)
+
+func part01(lines []string) int {
+	return 0
+}
+
+func part02(lines []string) int {
+	return 0
+}
+
+func Solve(input string) (int, int) {
+	lines, err := util.ReadFile(input)
+	if err != nil {
+		log.Fatal("Could not open file "+input, err)
+	}
+
+	return part01(lines), part02(lines)
+}
