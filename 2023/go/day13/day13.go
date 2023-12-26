a2bdf18f9287fa6b187cd03c841694b7f94ed693
Martin Rehberger <dev@kero.codes>
2023-12-26 20:20:20 +0700

    Day 13: Add attempt for first part worked on test data

--- 2023/go/day13/day13.go
+++ 2023/go/day13/day13.go
@@ -2,6 +2,7 @@ package day13
 
 import (
 	"log"
+	"math"
 	"strings"
 
 	"github.com/kingkero/adventofcode/2023/go/util"
@@ -51,8 +52,64 @@ func (original Matrix) rotate() *Matrix {
 	return &Matrix{data, original.height, original.width}
 }
 
-func part01(matrixes []*Matrix) int {
+func canMirrorColAt(col int, row []string) bool {
+	length := col + 1
+	half := int(math.Floor(float64(len(row)) / 2.0))
+	if col >= half {
+		length = len(row) - 1 - col
+	}
+
+	for i := 0; i < length; i++ {
+		if row[col-i] != row[col+i+1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (matrix *Matrix) getLeftMirrorCol() int {
+	// 0 cannot be a left mirror
+	// width and width - 1 cannot be left mirrors
+	possibles := make([]int, matrix.width-3)
+	for i := range possibles {
+		possibles[i] = i + 1
+	}
+
+	for _, row := range matrix.data {
+		var newPossibles []int
+		for _, possibleCol := range possibles {
+			if canMirrorColAt(possibleCol, row) {
+				newPossibles = append(newPossibles, possibleCol)
+			}
+		}
+
+		if len(newPossibles) == 0 {
+			return 0
+		}
+		possibles = newPossibles
+	}
+
+	if len(possibles) == 1 {
+		return possibles[0]
+	}
+
 	return 0
+}
+
+func part01(matrixes []*Matrix) int {
+	result := 0
+	for _, matrix := range matrixes {
+		if horizontalMirror := matrix.getLeftMirrorCol(); horizontalMirror > 0 {
+			result += horizontalMirror + 1
+			continue
+		}
+
+		if verticalMirror := matrix.rotate().getLeftMirrorCol(); verticalMirror > 0 {
+			result += (verticalMirror + 1) * 100
+		}
+	}
+	return result
 }
 
 func part02(lines []string) int {
