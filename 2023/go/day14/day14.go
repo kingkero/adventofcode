04ea749ea860ab8326e4a877346663c2999e518e
Martin Rehberger <dev@kero.codes>
2023-12-31 17:50:23 +0800

    Day 14: Parse platform

--- 2023/go/day14/day14.go
+++ 2023/go/day14/day14.go
@@ -2,15 +2,31 @@ package day14
 
 import (
 	"log"
+	"strings"
 
+	"github.com/gookit/goutil/dump"
 	"github.com/kingkero/adventofcode/2023/go/util"
 )
 
-func part01(lines []string) int {
+type Platform struct {
+	matrix [][]string
+}
+
+func NewPlatform(lines []string) *Platform {
+	matrix := make([][]string, len(lines))
+
+	for i, line := range lines {
+		matrix[i] = strings.Split(line, "")
+	}
+
+	return &Platform{matrix}
+}
+
+func part01(platform Platform) int {
 	return 0
 }
 
-func part02(lines []string) int {
+func part02(platform Platform) int {
 	return 0
 }
 
@@ -20,5 +36,9 @@ func Solve(input string) (int, int) {
 		log.Fatal("Could not open file "+input, err)
 	}
 
-	return part01(lines), part02(lines)
+	platform := NewPlatform(lines)
+
+	dump.P(platform)
+
+	return part01(*platform), part02(*platform)
 }
