4f7f53a6624a58fc92bb41ac95cd5afe8137f2a4
Martin Rehberger <dev@kero.codes>
2024-01-07 21:10:23 +0100

    Day 15: Solve part 01

--- 2023/go/day15/day15.go
+++ 2023/go/day15/day15.go
@@ -2,12 +2,27 @@ package day15
 
 import (
 	"log"
+	"strings"
 
 	"github.com/kingkero/adventofcode/2023/go/util"
 )
 
+func hash(data string) int {
+	result := 0
+
+	for _, element := range []rune(data) {
+		result += int(element)
+		result *= 17
+		result = result % 256
+	}
+
+	return result
+}
+
 func part01(lines []string) int {
-	return 0
+	sequences := strings.Split(lines[0], ",")
+	hashed := util.Map(sequences, hash)
+	return util.Sum(hashed)
 }
 
 func part02(lines []string) int {
