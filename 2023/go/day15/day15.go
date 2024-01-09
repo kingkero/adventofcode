69f45061f17402feafc03064a227547a76466135
Martin Rehberger <dev@kero.codes>
2024-01-09 21:07:30 +0100

    Day 15: Add hashmap for part 2

--- 2023/go/day15/day15.go
+++ 2023/go/day15/day15.go
@@ -2,8 +2,10 @@ package day15
 
 import (
 	"log"
+	"slices"
 	"strings"
 
+	"github.com/gookit/goutil/dump"
 	"github.com/kingkero/adventofcode/2023/go/util"
 )
 
@@ -23,7 +25,32 @@ func part01(lines []string) int {
 	return util.Sum(util.Map(strings.Split(lines[0], ","), hash))
 }
 
+type Lens struct {
+	label       string
+	focalLength int
+}
+
 func part02(lines []string) int {
+	parts := strings.Split(lines[0], ",")
+	hashMap := make(map[int][]*Lens)
+
+	for _, part := range parts {
+		equals := strings.Split(part, "=")
+
+		if len(equals) == 1 {
+			label := equals[0][:len(equals[0])-1]
+			hashMap[hash(label)] = slices.DeleteFunc(hashMap[hash(label)], func(lens *Lens) bool {
+				return lens.label == label
+			})
+			continue
+		}
+
+		lens := &Lens{equals[0], util.ParseInt(equals[1])}
+		hashMap[hash(equals[0])] = append(hashMap[hash(equals[0])], lens)
+	}
+
+	dump.P(hashMap)
+
 	return 0
 }
 
