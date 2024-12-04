55b107b7da5f67e738aa51424961e0be5a4182f5
Martin Rehberger <martin.rehberger@check24.de>
2024-12-04 19:01:06 +0100

    Solve day03 part 01

--- 2024/go/day03/day03.go
+++ 2024/go/day03/day03.go
@@ -1,7 +1,24 @@
 package day03
 
+import (
+	"regexp"
+	"strconv"
+
+	"github.com/kingkero/adventofcode/2024/go/util"
+)
+
 func Part01(input []string) string {
-	return ""
+	result := 0
+	r, _ := regexp.Compile("mul\\((\\d+),(\\d+)\\)")
+
+	for _, line := range input {
+		matches := r.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			result += util.ParseInt(match[1]) * util.ParseInt(match[2])
+		}
+	}
+
+	return strconv.Itoa(result)
 }
 
 func Part02(input []string) string {
