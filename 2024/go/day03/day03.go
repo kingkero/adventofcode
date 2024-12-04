36b571e0ffcd717959ec4f861c268bc01845fa8c
Martin Rehberger <martin.rehberger@check24.de>
2024-12-04 18:45:28 +0100

    Add empty day03 + day04

---
+++ 2024/go/day03/day03.go
@@ -0,0 +1,9 @@
+package day03
+
+func Part01(input []string) string {
+	return ""
+}
+
+func Part02(input []string) string {
+	return ""
+}
---
+++ 2024/go/day04/day04.go
@@ -0,0 +1,9 @@
+package day04
+
+func Part01(input []string) string {
+	return ""
+}
+
+func Part02(input []string) string {
+	return ""
+}
