1bb22037c5a4671f978df1c08986b00ae78433b8
Martin Rehberger <martin.rehberger@check24.de>
2024-12-08 16:39:01 +0100

    WIP: Start day 07 part 01

--- 2024/go/day07/day07.go
+++ 2024/go/day07/day07.go
@@ -1,7 +1,46 @@
 package day07
 
-func Part01(_ []string) string {
-	return ""
+import (
+	"strconv"
+	"strings"
+
+	"github.com/kingkero/adventofcode/2024/go/util"
+)
+
+const (
+	OperatorAdd uint8 = iota
+	OperatorMultiply
+
+	AllOperators
+)
+
+type Calibration struct {
+	result    int
+	parts     []int
+	operators []uint8
+}
+
+func Part01(input []string) string {
+	result := 0
+
+	for _, line := range input {
+		resultAndParts := strings.Split(line, ": ")
+
+		calibration := Calibration{
+			result: util.ParseInt(resultAndParts[0]),
+			parts:  util.Map(strings.Split(resultAndParts[1], " "), util.ParseInt),
+		}
+
+		if calibration.isSolvable() {
+			result += calibration.result
+		}
+	}
+
+	return strconv.Itoa(result)
+}
+
+func (calibration *Calibration) isSolvable() bool {
+	return false
 }
 
 func Part02(_ []string) string {
