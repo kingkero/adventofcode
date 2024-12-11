92670c1edd09dc88674ff59e7dec800b73bc3018
Martin Rehberger <martin.rehberger@check24.de>
2024-12-11 19:20:41 +0100

    Add smartBlink, still not much better than regular blink

--- 2024/go/day11/day11.go
+++ 2024/go/day11/day11.go
@@ -39,6 +39,45 @@ func blink(stones []uint64) []uint64 {
 	return result
 }
 
-func Part02(_ []string) string {
-	return ""
+func smartBlink(stones []uint64, amountOfBlinks int) uint64 {
+	result := uint64(0)
+
+	for _, stone := range stones {
+		result += smartBlinkStone(stone, amountOfBlinks)
+	}
+
+	return result
+}
+
+func smartBlinkStone(stone uint64, amountOfBlinks int) uint64 {
+	if amountOfBlinks == 0 {
+		return 1
+	}
+
+	a, b, split := applyRule(stone)
+
+	if split {
+		return smartBlinkStone(a, amountOfBlinks-1) + smartBlinkStone(b, amountOfBlinks-1)
+	}
+
+	return smartBlinkStone(a, amountOfBlinks-1)
+}
+
+func applyRule(stone uint64) (uint64, uint64, bool) {
+	if stone == 0 {
+		return 1, 0, false
+	}
+
+	val := strconv.Itoa(int(stone))
+	if len(val)%2 == 0 {
+		return util.ParseUint64(val[0 : len(val)/2]), util.ParseUint64(val[len(val)/2:]), true
+	}
+
+	return stone * 2024, 0, false
+}
+
+func Part02(input []string) string {
+	stones := util.Map(strings.Split(input[0], " "), util.ParseUint64)
+
+	return strconv.Itoa(int(smartBlink(stones, 75)))
 }
