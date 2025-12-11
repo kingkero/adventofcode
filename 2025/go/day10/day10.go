807e3465f724dbf9c81678fde6a1b10ed3e2e5da
Martin Rehberger <martin.rehberger@check24.de>
2025-12-12 00:46:30 +0100

    start using bitmaps for day10

--- 2025/go/day10/day10.go
+++ 2025/go/day10/day10.go
@@ -8,59 +8,49 @@ import (
 	"github.com/kingkero/adventofcode/2025/go/util"
 )
 
-type button struct {
-	targets []int
-}
-
-type lights []bool
-
-func (l lights) equals(other lights) bool {
-	if len(l) != len(other) {
-		return false
-	}
-
-	for i, light := range l {
-		if light != other[i] {
-			return false
-		}
-	}
-
-	return true
-}
-
-func (l lights) String() string {
-	builder := strings.Builder{}
-	for _, light := range l {
-		if light {
-			builder.WriteString("#")
-		} else {
-			builder.WriteString(".")
-		}
-	}
-
-	return builder.String()
-}
-
 func Part01(input []string) string {
 	for _, line := range input {
 		parts := strings.Split(line, " ")
 
-		// lights := make([]int, len(parts[0])-2)
-		goal := make(lights, len(parts[0])-2)
-		buttons := make([]button, len(parts)-2)
+		goal := 0
+		buttons := make([]int, len(parts)-2)
 
 		for pos, light := range parts[0][1 : len(parts[0])-1] {
-			goal[pos] = light == '#'
-		}
-
-		for pos, definition := range parts[1 : len(parts)-1] {
-			buttons[pos] = button{
-				targets: util.Map(strings.Split(strings.Trim(definition, "()"), ","), util.ParseInt),
+			if light == '#' {
+				goal |= 1 << pos
 			}
 		}
 
-		fmt.Println(goal)
-		fmt.Printf("%#v\n", buttons)
+		for buttonIndex, definition := range parts[1 : len(parts)-1] {
+			for _, pos := range util.Map(strings.Split(strings.Trim(definition, "()"), ","), util.ParseInt) {
+				buttons[buttonIndex] |= 1 << pos
+			}
+		}
+
+		fmt.Printf("%b\n", goal)
+		for _, button := range buttons {
+			fmt.Printf("%b\n", button)
+		}
+		fmt.Println("")
+
+		// lights := make([]int, len(parts[0])-2)
+		/*
+			goal := make(lights, len(parts[0])-2)
+			buttons := make([]button, len(parts)-2)
+
+			for pos, light := range parts[0][1 : len(parts[0])-1] {
+				goal[pos] = light == '#'
+			}
+
+			for pos, definition := range parts[1 : len(parts)-1] {
+				buttons[pos] = button{
+					targets: util.Map(strings.Split(strings.Trim(definition, "()"), ","), util.ParseInt),
+				}
+			}
+
+			fmt.Println(goal)
+			fmt.Printf("%#v\n", buttons)
+		*/
 	}
 
 	return strconv.Itoa(0)
